5c070a6f1d914fec7684bc11c2f32453a3534e36
Shreyash Jaiswal <80672539+shreyash-jais-02@users.noreply.github.com>
2023-10-05 21:46:16 +0530

    Added Day 3 code

---
+++ Build Array from Permutation (Day 3).py
@@ -0,0 +1,12 @@
+# Build Array from Permutation
+# https://leetcode.com/problems/build-array-from-permutation/
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [0] * n
+
+        for i in range(n):
+            ans[i] = nums[nums[i]]
+
+        return ans
