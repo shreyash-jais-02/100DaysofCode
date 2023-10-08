0b914a046567f159ac7e430cc33141e5ee2ba292
Shreyash Jaiswal <80672539+shreyash-jais-02@users.noreply.github.com>
2023-10-08 22:51:47 +0530

    Added Day 6 code

---
+++ Richest customer wealth (Day 6).py
@@ -0,0 +1,13 @@
+# Richest customer wealth
+
+#https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_wealth = 0
+        
+        for customer_accounts in accounts:
+            wealth = sum(customer_accounts)
+            max_wealth = max(max_wealth, wealth)
+        
+        return max_wealth
